b5456cd6425d72ea52944552e726f7dbb18c4883
Ayush Ranjan Sahoo <77970726+aysahoo@users.noreply.github.com>
2025-05-08 18:53:32 +0530

    Deleted the fix

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -6,7 +6,7 @@ import InputBox from '../Components/InputBox'
 const Chat = () => {
 
   return (
-<div className="relative w-full" style={{ height: '100svh' }}>
+<div className="relative w-full h-dvh">
   <img
     src={background}
     alt="background"
@@ -14,7 +14,7 @@ const Chat = () => {
   />
   <div className="relative z-10 flex flex-col h-full">
     <Navbar />
-    <div className="flex-1 p-4 overflow-y-auto chat-area">
+    <div className="flex-1 p-4 overflow-y-auto">
       {/* Chat area */}
     </div>
     <InputBox />
