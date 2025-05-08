aa0efae713e5f19a27a63459728e05e6f1be0574
Ayush Ranjan Sahoo <77970726+aysahoo@users.noreply.github.com>
2025-05-08 20:48:15 +0530

    Fixing again

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -6,26 +6,32 @@ import ChatArea from '../Components/ChatArea'
 const Chat = () => {
 
   return (
-<div className="relative flex-col w-full h-svh ">
-  <img
-    src={background}
-    alt="background"
-    className="absolute w-full h-full object-cover brightness-50"
-  />
-  <div className='relative z-10 flex flex-col h-dvh'>
-  <div>
-    <Navbar />
-  </div>
-    <div className="flex-1 p-4">
-      <ChatArea />
+    <div className="relative w-full h-svh overflow-hidden">
+      {/* Background Image */}
+      <img
+        src={background}
+        alt="background"
+        className="absolute inset-0 w-full h-full object-cover brightness-50"
+      />
+  
+      {/* Main content on top of background */}
+      <div className="relative z-10 flex flex-col h-full">
+        {/* Navbar */}
+        <Navbar />
+  
+        {/* Chat Area (scrollable) */}
+        <div className="flex-1 overflow-y-auto p-4">
+          <ChatArea />
+        </div>
+  
+        {/* Input Box at bottom */}
+        <div className="p-4">
+          <InputBox />
+        </div>
+      </div>
     </div>
-    <div>
-    <InputBox />
-  </div>
-  </div>
-</div>
-
-  )
+  );
+  
 }
 
 export default Chat
